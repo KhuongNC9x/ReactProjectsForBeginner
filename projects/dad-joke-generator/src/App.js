831fdaa6efc12424281e5d7240382a3b3a30b9b9
KhuongNC <khuongnc@rikkeisoft.com>
2023-03-07 16:42:21 +0700

    Add code for generating joke

--- projects/dad-joke-generator/src/App.js
+++ projects/dad-joke-generator/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+function JokeButton({ children }) {
+  const [joke, setJoke] = useState("Let's have a funny joke!!!");
+
+  async function getJoke() {
+    const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
+    const apiUrl = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+    const option = {
+      method: "GET",
+      headers: {
+        "X-Api-Key": apiKey,
+      },
+    };
+
+    try {
+      const response = await fetch(apiUrl, option);
+      const data = await response.json();
+      setJoke(data[0].joke);
+    } catch (error) {
+      console.log("Dad Joke Generator " + error);
+      setJoke("An error happened, try again later");
+    }
+  }
+
+  return (
+    <>
+      <p className="joke">{joke}</p>
+      <button className="btn" onClick={getJoke}>
+        {children}
+      </button>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="heading">Dad Joke Generator</h1>
+      <JokeButton>Tell me a joke</JokeButton>
     </div>
   );
 }
