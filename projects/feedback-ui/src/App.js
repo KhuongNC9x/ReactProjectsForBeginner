bbb87dcb5e1a5eee1c6adb2b3307f676289278ee
KhuongNC <khuongnc@rikkeisoft.com>
2023-03-09 10:15:50 +0700

    Add code for feedback-ui project

--- projects/feedback-ui/src/App.js
+++ projects/feedback-ui/src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+function RatingBlock({ imgSrc, status, onClick }) {
+  return (
+    <div className="rating" onClick={onClick}>
+      <img src={imgSrc} alt={status} />
+      <small>{status}</small>
+    </div>
+  );
+}
+
+function ChildOfContainerBlock() {
+  const [selectedRating, setSelectedRating] = useState("");
+  let feedback = "";
+
+  function selectRating(e) {
+    feedback = e.target.innerText || e.target.parentNode.innerText;
+  }
+
+  function sendReview() {
+    setSelectedRating("");
+    setSelectedRating(feedback);
+  }
+
+  if (selectedRating === "") {
+    return (
+      <>
+        <h1 className="heading">Feedback UI</h1>
+        <div className="ratings-container" id="ratings-container">
+          <RatingBlock
+            imgSrc={"https://cdn-icons-png.flaticon.com/512/166/166527.png"}
+            status={"Unhappy"}
+            onClick={selectRating}
+          ></RatingBlock>
+          <RatingBlock
+            imgSrc={"https://cdn-icons-png.flaticon.com/512/1791/1791385.png"}
+            status={"Neutral"}
+            onClick={selectRating}
+          ></RatingBlock>
+          <RatingBlock
+            imgSrc={"https://cdn-icons-png.flaticon.com/512/166/166538.png"}
+            status={"Satisfied"}
+            onClick={selectRating}
+          ></RatingBlock>
+        </div>
+        <button className="btn" onClick={sendReview}>
+          Send Review
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <strong>Thank you!</strong>
+        <br />
+        <br />
+        <strong>Feedback:{selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer support.</p>
+      </>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" id="container">
+      <ChildOfContainerBlock></ChildOfContainerBlock>
     </div>
   );
 }
